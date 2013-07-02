d37f00df11c8e5e012059f0fa411ff9e5ebd42c7
Jake C <jake@darks.me>
2013-07-02 13:55:53 -0400

    added inductance

--- electrical sim/esim.m
+++ electrical sim/esim.m
@@ -33,11 +33,20 @@ for n=1:ncap
     cap.out(n) = input('Output Node = ');
     cap.mag(n) = input('Capacitance = ');
 end
+
+nind = input('Input number of inductors = ');
+if nind==0; ind.in=[]; ind.out=[]; end
+for n=1:nind
+    fprintf('Capacitor %g \n',n);
+    ind.in(n) = input('Input Node = ');
+    ind.out(n) = input('Output Node = ');
+    ind.mag(n) = input('Inductance = ');
+end
 %calculate number of nodesand branches (components)
 nnodes = max([ max([vsource.out]) max([resistor.out]) max([cap.out]) ...
-    max([vsource.in]) max([resistor.in]) max([cap.in]) ]); 
+    max([vsource.in]) max([resistor.in]) max([cap.in]) max([ind.in]) max([ind.out]) ]);
 
-matsize= nnodes + size(vsource.mag,1);%plus buffer columns for sources
+matsize= nnodes + size(vsource.mag,1) + size(ind.in,1);%plus buffer columns for sources
 
 condmat = spalloc(matsize,matsize,2*size(resistor.in,2));
 capmat = spalloc(matsize,matsize,2*size(cap.in,2));
@@ -46,10 +55,10 @@ voltage = zeros(matsize,1);
 %now we put all the components in their respective matrices
 for n=1:nvoltage
     %for standard vector of voltage sources
-    if vsource.in(n)~=0; 
+    if vsource.in(n)~=0;
         condmat(nnodes+n,vsource.in(n)) = -1;
     end
-    if vsource.out(n)~=0; 
+    if vsource.out(n)~=0;
         condmat(nnodes+n,vsource.out(n)) = 1;
         condmat(vsource.out(n),nnodes+n) = 1;
     end
@@ -67,44 +76,67 @@ for n=1:nresist
     end
     if resistor.in(n)~=0 && resistor.out(n)~=0
         condmat(resistor.in(n),resistor.out(n)) = condmat(resistor.in(n),resistor.out(n)) - 1/resistor.mag(n);
-        condmat(resistor.out(n),resistor.in(n))=condmat(resistor.out(n),resistor.in(n)) - 1/resistor.mag(n);       
+        condmat(resistor.out(n),resistor.in(n))=condmat(resistor.out(n),resistor.in(n)) - 1/resistor.mag(n);
     end
 end
-    for n=1:ncap
-        if cap.in(n)~=0; 
-            capmat(cap.in(n),cap.in(n))= capmat(cap.in(n),cap.in(n)) + cap.mag(n);
-        end
-        if cap.out(n)~=0; 
-            capmat(cap.out(n),cap.out(n))= capmat(cap.out(n),cap.out(n)) + cap.mag(n);        
-        end
-        if cap.in(n)~=0 && cap.out(n)~=0;
-           capmat(cap.in(n),cap.out(n)) = capmat(cap.in(n),cap.out(n)) - cap.mag(n);
-           capmat(cap.out(n),cap.in(n)) = capmat(cap.out(n),cap.in(n)) - cap.mag(n);
-            
-        end
+for n=1:ncap
+    if cap.in(n)~=0;
+        capmat(cap.in(n),cap.in(n))= capmat(cap.in(n),cap.in(n)) + cap.mag(n);
     end
-    %do the LU decomposition
-    [LL,LU,LP] = lu((capmat + deltat*condmat));
-       % x= voltage(:,n+1
-    %A=(capmat + deltat*condmat)
-    %b=(capmat*voltage(:,n)+source*deltat)
-    %y=LL\LP*b
-    %x=LU\(LL\LP*b)
-    
-    
-    %time marching -- should have itcheck for convergence in solid-state system)
-    for n=1:timesteps
-        %voltage(:,n+1)= ((capmat + deltat*condmat)\(capmat*voltage(:,n)+source*deltat));
-        voltage(:,n+1)= LU\(LL\LP*(capmat*voltage(:,n)+source*deltat));
+    if cap.out(n)~=0;
+        capmat(cap.out(n),cap.out(n))= capmat(cap.out(n),cap.out(n)) + cap.mag(n);
+    end
+    if cap.in(n)~=0 && cap.out(n)~=0;
+        capmat(cap.in(n),cap.out(n)) = capmat(cap.in(n),cap.out(n)) - cap.mag(n);
+        capmat(cap.out(n),cap.in(n)) = capmat(cap.out(n),cap.in(n)) - cap.mag(n);
+        
+    end
+end
 
-        %for setting voltage in the voltage vector
-        
-        
+for n=1:nind
+    if ind.in(n)~=0;
+        %         capmat(cap.in(n),cap.in(n))= capmat(cap.in(n),cap.in(n)) + cap.mag(n);
+         condmat(nnodes+nvoltage+n,ind.in(n)) = -1;
+         condmat(ind.in(n),nnodes+nvoltage+n) = 1;
     end
-    figure;
-    hold on
-    for n=1:nnodes
-        plot(voltage(n,:),n*deltat);
+    if ind.out(n)~=0;
+        %         capmat(cap.out(n),cap.out(n))= capmat(cap.out(n),cap.out(n)) + cap.mag(n);
+         condmat(ind.out(n),nnodes+nvoltage+n) = -1;
+        condmat(nnodes+nvoltage+n,ind.out(n)) = 1;
     end
+    if ind.in(n)~=0 && ind.out(n)~=0;
+        %         indmat(ind.in(n),ind.out(n)) = indmat(cap.in(n),cap.out(n)) - cap.mag(n);
+        %         indmat(ind.out(n),ind.in(n)) = indmat(cap.out(n),cap.in(n)) - cap.mag(n);
+    end
+    capmat(nnodes+nvoltage+n,nnodes+nvoltage+n)=ind.mag(n);
     
     
+end
+
+
+
+
+%do the LU decomposition
+[LL,LU,LP] = lu((capmat + deltat*condmat));
+% x= voltage(:,n+1
+%A=(capmat + deltat*condmat)
+%b=(capmat*voltage(:,n)+source*deltat)
+%y=LL\LP*b
+%x=LU\(LL\LP*b)
+
+voltage(:,1)=source;
+%time marching -- should have itcheck for convergence in solid-state system)
+for n=1:timesteps
+    %voltage(:,n+1)= ((capmat + deltat*condmat)\(capmat*voltage(:,n)+source*deltat));
+    voltage(:,n+1)= LU\(LL\LP*(capmat*voltage(:,n)+source*deltat));
+    %for setting voltage in the voltage vector
+    
+    
+end
+figure;
+hold on
+for n=1:nnodes
+    plot(voltage(n,:),n*deltat);
+end
+
+
