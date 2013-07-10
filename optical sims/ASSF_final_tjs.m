b0df96b476d1141adb29041dec0aa24e42ce57ae
Tom Smy <tjssmy@gmail.com>
2013-07-09 20:27:12 -0400

    update

--- optical sims/ASSF_final_tjs.m
+++ optical sims/ASSF_final_tjs.m
@@ -2,12 +2,33 @@
 % idu/ dz - sgn(beta2) /2 d^2u/d(tau) ^2+N ^2*|u| ^2*|u| ^2u=0
 % Modified from version written by Govind P. Agrawal in Marcg 2005 for the NLFO book
 
+%  idu/ dz - sgn(beta2) /2 d^2u/d(tau) ^2+N ^2*|u|^2 * u=0
+% SS
+%     du/dz = i sgn(beta2)/2 d^2u/dtau^2 + i N^2 *|u|^2 u  
+%     direct integration in z:  u(x0+dz) = u(x0) exp(O dz) 
+%     1)  In freq domain: O = FT[i sgn(beta2)/2 d^2u/dtau^2] = i sgn(beta2)/2 omega^2
+%         lindisp = exp(i sgn(beta2)/2 omega^2 dz)
+%         u(f,x0+dz) = u(f,x0) * lindisp
+%     2)  In time domain 0 = N ^2*|u| ^2 and u(t) = fff(u(f)
+%         u(t,x0+dz) = u(t,x0) exp(i N ^2 *dz |u| ^2)
+%
+% FD:
+%    du/dz = i sgn(beta2)/2 d^2u/dtau^2 + i N^2 *|u|^2 u  
+% 1) disp
+%         i sgn(beta2)/2 d^2u/dtau^2 = i sgn(beta2)/2 G u
+%                    du/dz = i sgn(beta2)/2 G u
+%                    du(n)/dz  = alpha * (u(n+1) - 2 u(n) + u(n-1))
+%                    (u - up)/dz = i sgn(beta2)/2 G u
+%                    (I - dz i sgn(beta2)/2 G) u = up
+%                    u = (1 - dz i sgn(beta2)/2 G)\up
+%   2) nonlinear
+%       u(t,x0+dz) = u(t,x0) exp(i N ^2 *dz |u| ^2)
 
 %FFFFFFIIIIXXXXXXXX
 %Does not work for non zero sigma value if only one wave
 %sigma set to 0 if only one wave. should recheck math to see if multiwave
 %approach is valid.
-clear all;
+clear;
 close all;
 
 set(0,'DefaultFigureWindowStyle','docked')
@@ -67,7 +88,7 @@ else
     
     % specify input parameters (Normalized)
     beta2 = -1; % this captures actual beta2 value in Ld normalization
-    N = 1;
+    N = 0.0;
     mshape = 0;%input('m = 0 for sech, m > 0 for super-Gaussian = '); % decided on input wave
     alpha = 0;
     ss = 0;
@@ -81,7 +102,6 @@ nt = 1024; Tmax = 32; % FFT points and window size for graphs
 step_num = 200;
 deltaz = distance/step_num; % step size in z
 dtau = (2 * Tmax)/nt; % step size in tau (radians from z)
-
 
 % tau and omega arrays
 tau = (-nt/2:nt/2-1) * dtau; % temporal grid
@@ -97,37 +117,68 @@ end
 lindispersion = exp(0.5i*beta2*omega.^2*deltaz-0.5*alpha*deltaz); %phase factor, includes alpha
 hhz= 1i*N^2 *deltaz; %nonlinear phase factor
 temp = uu.*exp(abs(uu).^2*hhz/2); % note hhz/2 first step, hhz (non-linear) decay
+tempFD = temp;
+
+G = zeros(nt);
+D = eye(nt);
+for j=1:nt
+    if j == 1
+        G(j,nt) = 1;
+        G(j,j) = - 2;
+        G(j,j+1) = 1;
+    elseif j == nt
+            G(j,j-1) = 1;
+            G(j,j) = -2;
+            G(j,1) = 1;
+    else
+        G(j,j-1) = 1;
+        G(j,j) = -2;
+        G(j,1) = 1;
+    end
+end
+G = G/(dtau*dtau);
+
+LG =  (D - deltaz*0.5i*beta2*G);
+[l,u] = lu(LG);
+
+duudt = fft(ifft(temp).*omega.^2);
+duuFDdt = G*tempFD';
+
 
 for n=1:step_num
-    sumwaves=0; % initializing summation of square of every wave
-    sumwaves=sumwaves+sigma.*(abs(uu).^2 );
-    
     %calculating derivatives of wave in frequency and time domain
     
-    duu_dt=ifft(1i.*omega.*temp); % frequency domain(as ifft and fft are symmetrical)
-    duu_dt_star=ifft(1i.*omega.*fft(conj(temp))); % time domain
     
-    % calculating self steepening
-    selfsteepen=exp((2.*conj(temp).*duu_dt + uu.*duu_dt_star).*deltaz.*ss);
-    
+    plot(abs(temp).^2);
+    hold
+    plot(abs(tempFD).^2,'r+');
+
+    % Line Disp 
     f_temp = ifft(temp).*lindispersion; % step using dispersion decay (goes from wave 1-number; taking advantage of the way this loop works)
-    
     uu = fft(f_temp); %amplitude updates the wave at that step (after step at line 73)
+    % 
+    uuFD = (u\(l\(tempFD')))';
     
-    % subtracting intial wave from sumwaves as in the math all waves
-    % but the first contribute sigma their squares and first wave
-    % contributes 1
-    % (recall the typical case when sigma is 2; every wave but first contributes twice their squares)
-    temp = uu.*exp((sumwaves-(sigma-1).*abs(uu).^2).*hhz).* selfsteepen; % applying self-steepening and non linear dispersion
+    plot(abs(uu).^2,'bo');
+    plot(abs(uuFD).^2,'g*');
+    
+    drawnow();
+    pause(0.1)
+    
+    %non linear dispersion
+    temp = uu.*exp((abs(uu).^2).*hhz); 
+    tempFD = uuFD.*exp((abs(uuFD).^2).*hhz); 
     
     f_temp = ifft(temp);% stores the frequency domain; used later to store final wave
-    
-    
+        
     tempplot = fftshift(ifft(uu)).*(nt*dtau)/sqrt(2*pi);% plotted spectrum
+    tempplotFD = fftshift(ifft(uuFD)).*(nt*dtau)/sqrt(2*pi);% plotted spectrum
     
     % logs data at every step (except last)so it may be plotted later
     wavespace(n+1,:)= abs(uu).^2;
+    wavespaceFD(n+1,:)= abs(uuFD).^2;
     wavefreq(n+1,:)=abs(tempplot).^2;
+    wavefreqFD(n+1,:)=abs(tempplotFD).^2;
     
 end
 
@@ -138,8 +189,6 @@ Toff = Za*beta1*Ld; %ps
 
 t = 512*dtau*T0; 
 TT = Toff + t;
-
-
 
 % plotting wave envelope
 subplot(2,1,1)
